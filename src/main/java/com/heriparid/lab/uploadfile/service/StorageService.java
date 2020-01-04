bcd894029a751a926b47077debacb838ec6e2cc9
Heri Parid Pardian <heri.par@ascendcorp.com>
2020-01-04 21:04:06 +0700

    enable kubernetes: - enable Deployment to bind with volume -> pvc - define pvc - define pv

--- src/main/java/com/heriparid/lab/uploadfile/service/StorageService.java
+++ src/main/java/com/heriparid/lab/uploadfile/service/StorageService.java
@@ -6,16 +6,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 @Service
 @Slf4j
 public class StorageService implements IStorageService {
-
 
     private final String path;
 
@@ -24,6 +25,14 @@ public class StorageService implements IStorageService {
             @Value("${upload.path}") String path){
 
         this.path = path;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        if(!Files.exists(Paths.get(path))) {
+            Path createdPath = Files.createDirectory(Paths.get(path));
+            log.info("Directory: {} was created", createdPath.toAbsolutePath());
+        }
     }
 
     @Override
@@ -50,8 +59,9 @@ public class StorageService implements IStorageService {
             log.info("File: {} - {} Size: {} was uploaded to server", originalName, contentType, size);
             result = true;
         }catch (IOException e){
-            log.error("Failed to store file {}", file.getName());
+            log.error("Failed to store file {}", file.getOriginalFilename());
             // throw RuntiimeExcetion??
+            e.printStackTrace();
         }
 
         return result;
